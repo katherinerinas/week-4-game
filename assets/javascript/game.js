e655e69252f77022bf1301bdc802f1315a25ee81
Katherine Rinas <katherinerinas@Katherine-Rinass-MacBook-Pro.local>
2018-01-23 20:59:04 -0700

    set up the html and css to begin with as well as pseudocoded javascript gameplan

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+//assign random number generator-computer
+
+//find pictures of crystals-set them to the page
+
+//assign each crystal an array and for loop that runs through the values 1-12
+
+//set if then statements to where if the userscore =computerscore: alerts : you win!
+
+//set it to where if players score > computerscore: player loses
+
+//set a counter to keep track of the user's total score
