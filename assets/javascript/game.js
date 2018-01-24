ee4c29283c618e7610557214270c297af126258a
Katherine Rinas <katherinerinas@Katherine-Rinass-MacBook-Pro.local>
2018-01-24 16:49:08 -0700

    added things before trying something else

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,16 @@
-//assign random number generator-computer
 
-//find pictures of crystals-set them to the page
 
-//assign each crystal an array and for loop that runs through the values 1-12
+
+//computer random number
+//
+//assign each crystal an array and for loop that runs through the values 1-12:
+// (trouble getting the loops to work) {random numbers, four random numbers btween 1/12
+	//wins, losses, total score: 
+//figure out what crystal was clicked and store in variable
+//store crystal value in variable crystalclickedvalue
+
+//add crystal clicked value to total score
+
 
 //set if then statements to where if the userscore =computerscore: alerts : you win!
 
@@ -10,16 +18,53 @@
 
 //set a counter to keep track of the user's total score
 
- 
-  var crystalValue = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", 
-  "11", "12"];
+//reset game without having to reset entire page
+//(reset wins, losses, and total score to 0)
 
-  for (var i = 0; i < crystalValue.length; i++){
-  	
-  
-$("#crystala").on("click", function(){
-	alert("The current value is" + crystalValue[i]);
+//reset computer random number, reset. crystal array
+//update page to put in new wins, random number, user total score
+
+//if win, same thing, but add 1 to losses
+
+
+//put wins on page
+//put losses on page
+//put computerRandomNumberon page
+//put userTotalScore on page
+
+
+//var computerRandomNumber;
+
+//reset function
+
+//reset();
+
+
+$(document).ready(function(){
+
+var counter = 0
+var wins= 0
+var losses= 0
+var Totalwins= 0
+
+
+var numberGoal = Math.floor(Math.random() * ((120-19)+1) + 19);
+      alert("The value that you want is:   "  +  numberGoal);
+
+      $("#computernumber").text(numberGoal);
+
+ 
+ });
+
+ var crystalValues = [9, 5, 2, 7];
+
+for (var i = 0; i<crystalValues.length; i++) {
 	
 
-});
-}
+
+
+$("#crystala").on("click", function() {
+	alert (crystalValues[i]);
+})
+
+};
