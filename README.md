c6ba71efbee321623ae1963d6275920f58ee42a2
Katherine Rinas <katierinas@me.com>
2018-03-11 13:57:04 -0600

    added the read me

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # week-4-game
+
+Users will be tasked with a random number at the beginning of the game.
+There are four crystals, each with different hidden values. They may Click on a crystal to add a specific amount of points to the total score. If a user matches their total score to the random number, the game is won. If their total score goes above the random number, they lose.  The values of each crystal will change with each new game.
--- assets/javascript/gamex2.js
+++ assets/javascript/gamex2.js
@@ -1,12 +1,11 @@
 $(document).ready(function(){
 
-    //define variables
+    
     var wins= 0
     var losses= 0
     var score =0
 
 
-    //generate random number for computer, push to html
     var numberGoal = Math.floor(Math.random() * ((120-19)+1) + 19);
     alert("The value that you want is:   "  +  numberGoal);
 
@@ -27,7 +26,7 @@ $(document).ready(function(){
     $("#crystal2").attr("data-crystalvalue", crystalValues[2]);
     $("#crystal3").attr("data-crystalvalue", crystalValues[3]);
 
-//setting values to each crystal
+
 
         
        
@@ -45,7 +44,7 @@ $(document).ready(function(){
 
         score = score + crystalValue;
              console.log(score);
-        //This is the counter # section: 
+      
 
         $("#yourscore").text(score);
 
